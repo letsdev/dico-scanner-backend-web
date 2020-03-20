1381e482ae27b30ab4efc9ac88dd94a764bd98fa
Christian Wack - let's dev <christian.wack@letsdev.de>
2020-03-20 23:39:28 +0100

    added methods

--- src/main/java/de/letsdev/products/dico/scanner/backend/service/LocationService.java
+++ src/main/java/de/letsdev/products/dico/scanner/backend/service/LocationService.java
@@ -1,8 +1,21 @@
 package de.letsdev.products.dico.scanner.backend.service;
 
+import de.letsdev.products.dico.scanner.backend.db.Location;
+import de.letsdev.products.dico.scanner.backend.db.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
 
 @Service
 public class LocationService {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    public void savePosition(Timestamp timestamp, Location){
+
+
+    }
 
 }
