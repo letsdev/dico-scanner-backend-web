ee0288d9544dac1cff57eef7c4efea2b25b4ca93
Tobias Wächter <tobias.waechter@letsdev.de>
2020-03-22 00:10:29 +0100

    added time zone

--- src/main/java/de/letsdev/products/dico/scanner/backend/scheduler/SymptomsScheduler.java
+++ src/main/java/de/letsdev/products/dico/scanner/backend/scheduler/SymptomsScheduler.java
@@ -29,13 +29,13 @@ public class SymptomsScheduler {
 
     Logger log = LoggerFactory.getLogger(SymptomsScheduler.class);
 
-    @Scheduled(cron = "0 0 7 * * *")
+    @Scheduled(cron = "0 0 7 * * *", zone="Europe/Berlin")
     public void symptomsReminder() {
         log.info("daily reminder scheduler started");
         List<Device> devices = deviceService.getAllDevices();
 
         String title = environment.getProperty("push.message.reminder.title", "Tägliche Erinnerung");
-        String message = environment.getProperty("push.message.reminder.message", "Bitte die Symptome für heute eingeben.")
+        String message = environment.getProperty("push.message.reminder.message", "Bitte die Symptome für heute eingeben.");
 
         for(Device device : devices) {
             try {
