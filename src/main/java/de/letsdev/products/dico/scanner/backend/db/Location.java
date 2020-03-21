c434a2ad1286f2da29ea7fcabb5fa5592e0df434
Tobias Wächter <tobias.waechter@letsdev.de>
2020-03-21 01:35:45 +0100

    added generation value

--- src/main/java/de/letsdev/products/dico/scanner/backend/db/Location.java
+++ src/main/java/de/letsdev/products/dico/scanner/backend/db/Location.java
@@ -1,12 +1,15 @@
 package de.letsdev.products.dico.scanner.backend.db;
 
 import org.springframework.data.annotation.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 import java.sql.Timestamp;
 
 public class Location {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Timestamp timestamp;
 
     float lon;
