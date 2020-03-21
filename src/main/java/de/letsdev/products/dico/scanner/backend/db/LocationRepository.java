22a52d05067cd52778651b030e7a4535cee82066
Tobias Wächter <tobias.waechter@letsdev.de>
2020-03-21 16:10:04 +0100

    added timestamp query

--- src/main/java/de/letsdev/products/dico/scanner/backend/db/LocationRepository.java
+++ src/main/java/de/letsdev/products/dico/scanner/backend/db/LocationRepository.java
@@ -3,10 +3,13 @@ package de.letsdev.products.dico.scanner.backend.db;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
     List<Location> findAllByDeviceUuid(String uuid);
+
+    List<Location> findAllByTimestampAfter(Timestamp timestamp);
 }
--- src/main/java/de/letsdev/products/dico/scanner/backend/service/LocationService.java
+++ src/main/java/de/letsdev/products/dico/scanner/backend/service/LocationService.java
@@ -4,14 +4,17 @@ import de.letsdev.products.dico.scanner.backend.Position;
 import de.letsdev.products.dico.scanner.backend.db.Device;
 import de.letsdev.products.dico.scanner.backend.db.Location;
 import de.letsdev.products.dico.scanner.backend.db.LocationRepository;
-import org.hibernate.event.spi.PostCollectionRecreateEvent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class LocationService {
@@ -19,7 +22,10 @@ public class LocationService {
     @Autowired
     private LocationRepository locationRepository;
 
-    public void savePosition(Device device, Position position) {
+    @Autowired
+    private Environment environment;
+
+    public Location savePosition(Device device, Position position) {
 
         Instant instant = Instant.parse(position.getTimestamp());
         Timestamp timestamp = Timestamp.from(instant);
@@ -31,12 +37,14 @@ public class LocationService {
         location.setAccuracy(position.getAccuracy());
         location.setDevice(device);
         locationRepository.save(location);
+
+        return location;
     }
 
     public List<Position> findAllByDeviceUuid(String uuid) {
 
         List<Location> locations = locationRepository.findAllByDeviceUuid(uuid);
-        List<Position> positions = new ArrayList<Position>();
+        List<Position> positions = new ArrayList<>();
 
         for (Location location : locations) {
             Position position = new Position();
@@ -48,5 +56,24 @@ public class LocationService {
         }
 
         return positions;
+    }
+
+    @Async
+    public CompletableFuture<List<Location>> findNearlyLocations(Location location) {
+        //TODO get allLocations withing 14 day (config value)
+        String maxDays = environment.getProperty("coscan.search.maximum.days");
+        int max = Integer.parseInt(maxDays);
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(location.getTimestamp());
+        cal.add(Calendar.DAY_OF_WEEK, -max);
+        Timestamp referenceTimestamp = new Timestamp(cal.getTime().getTime());
+
+        List<Location> locations = locationRepository.findAllByTimestampAfter(referenceTimestamp);
+
+        double latFrom = location.getLat();
+        double lonFrom = location.getLon();
+
+        return null;
     }
 }
