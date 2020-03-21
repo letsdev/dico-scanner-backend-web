a8f271120f51858f95060c61b1493acf65c96894
Christian Wack - let's dev <christian.wack@letsdev.de>
2020-03-21 13:41:07 +0100

    added pushservice

--- src/main/java/de/letsdev/products/dico/scanner/backend/db/LocationRepository.java
+++ src/main/java/de/letsdev/products/dico/scanner/backend/db/LocationRepository.java
@@ -4,7 +4,7 @@ import org.apache.catalina.startup.ClassLoaderFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class LocationRepository {
+public class  LocationRepository {
 
 
 
