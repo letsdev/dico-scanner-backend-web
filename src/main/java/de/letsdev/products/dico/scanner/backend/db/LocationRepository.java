21429a094ca5b07e85c4b55ed3043bff82802b84
Tobias Wächter <tobias.waechter@letsdev.de>
2020-03-22 01:00:46 +0100

    removed old query

--- src/main/java/de/letsdev/products/dico/scanner/backend/db/LocationRepository.java
+++ src/main/java/de/letsdev/products/dico/scanner/backend/db/LocationRepository.java
@@ -1,6 +1,5 @@
 package de.letsdev.products.dico.scanner.backend.db;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,7 +12,6 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
 
     List<Location> findAllByDeviceUuid(String uuid);
 
-//    @Query(nativeQuery = true, value = "SELECT l.* FROM location l JOIN device d on l.device_id = d.id JOIN test_state ts on ts.device_id = d.id WHERE ts.state=1 AND l.timestamp BETWEEN :refDate1 AND :refDate2;")
-    @Query(nativeQuery = true, value = "SELECT * FROM location l JOIN device d on l.device_id = d.id JOIN test_state ts on ts.device_id = d.id WHERE ts.state=1 AND l.timestamp BETWEEN :timestampBefore AND :timestampAfter AND d.id != :deviceId")
+    @Query(nativeQuery = true, value = "SELECT * FROM location l JOIN device d on l.device_id = d.id JOIN test_state ts on ts.device_id = d.id WHERE ts.state=1 AND l.timestamp BETWEEN :timestampBefore AND :timestampAfter AND d.id != :deviceId ")
     List<Location> findAllByTestResultPositiveAndTimestampBetweenAnd(Timestamp timestampBefore, Timestamp timestampAfter, Long deviceId);
 }
